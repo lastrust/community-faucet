bbf30a7811184b99b0ab2d6bb2793d661a09916f
inaridiy <inainainariinainari@gmail.com>
2022-03-28 12:11:36 +0900

    エラー確認

--- app/src/pages/api/drop.ts
+++ app/src/pages/api/drop.ts
@@ -12,46 +12,38 @@ type BodyType = {
 const allowedTime = 1000 * 60 * 1; //
 
 const tokenUri = async (req: NextApiRequest, res: NextApiResponse) => {
-  try {
-    invariant(req.method == "POST", "must be POST method");
+  invariant(req.method == "POST", "must be POST method");
 
-    const { message, signature } = req.body as BodyType;
-    invariant(message && signature, "Body is not correct.");
+  const { message, signature } = req.body as BodyType;
+  invariant(message && signature, "Body is not correct.");
 
-    const [, , timeLine, addressLine] = message.split("\n");
-    const [time, address] = [timeLine.slice(6), addressLine.slice(9)];
+  const [, , timeLine, addressLine] = message.split("\n");
+  const [time, address] = [timeLine.slice(6), addressLine.slice(9)];
 
-    const recoveredAddress = ethers.utils.verifyMessage(message, signature);
-    const isMatchAddress =
-      recoveredAddress.toLowerCase() === address.toLowerCase();
-    const isInTime = Date.now() - Number(time) < allowedTime;
-    invariant(
-      isMatchAddress && isInTime,
-      JSON.stringify({ signature, time, address, recoveredAddress, isInTime })
-    );
+  const recoveredAddress = ethers.utils.verifyMessage(message, signature);
+  const isMatchAddress =
+    recoveredAddress.toLowerCase() === address.toLowerCase();
+  const isInTime = Date.now() - Number(time) < allowedTime;
+  invariant(isMatchAddress && isInTime, "Invalid signature");
 
-    invariant(
-      process.env.PRIVATE_KEY && process.env.NEXT_PUBLIC_CONTRACT_ADDRESS,
-      "env not found"
-    );
-    const provider = new ethers.providers.JsonRpcProvider(
-      targetChain().rpcUrls[0]
-    );
-    const signer = new ethers.Wallet(process.env.PRIVATE_KEY, provider);
-    const contract = StudentFaucet__factory.connect(
-      process.env.NEXT_PUBLIC_CONTRACT_ADDRESS,
-      signer
-    );
+  invariant(
+    process.env.PRIVATE_KEY && process.env.NEXT_PUBLIC_CONTRACT_ADDRESS,
+    "env not found"
+  );
+  const provider = new ethers.providers.JsonRpcProvider(
+    targetChain().rpcUrls[0]
+  );
+  const signer = new ethers.Wallet(process.env.PRIVATE_KEY, provider);
+  const contract = StudentFaucet__factory.connect(
+    process.env.NEXT_PUBLIC_CONTRACT_ADDRESS,
+    signer
+  );
 
-    const tx = await contract.drop(address, {
-      gasLimit: "100000",
-    });
+  const tx = await contract.drop(address, {
+    gasLimit: "100000",
+  });
 
-    res.json({ status: "success" });
-  } catch (e) {
-    console.log(e);
-    res.status(500).json({ status: String(e) });
-  }
+  res.json({ status: "success" });
 };
 
 export default tokenUri;
