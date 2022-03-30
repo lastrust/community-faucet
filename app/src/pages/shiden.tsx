1f31ef0e6f8bea0961afc768c5973425781de7eb
inaridiy <inainainariinainari@gmail.com>
2022-03-31 00:15:23 +0900

    メンテナンス

--- app/src/pages/shiden.tsx
+++ app/src/pages/shiden.tsx
@@ -1,5 +1,4 @@
 import DefaultLayout from "@/components/DefaultLayout";
-import ShidenHero from "@/components/hero/ShidenHero";
 import StatsBase from "@/components/StatsBase";
 import type { NextPage } from "next";
 import { NextSeo } from "next-seo";
@@ -16,7 +15,10 @@ const Home: NextPage = () => {
         }}
       />
       <DefaultLayout theme={["lofi", "night"]}>
-        <ShidenHero />
+        {/* <ShidenHero /> */}
+        <h1 className="text-center text-6xl font-bold text-red-500 sm:mt-32">
+          Under Maintenance
+        </h1>
         <div className="mx-2 flex justify-center">
           <StatsBase type="shiden" symbol="SDN" />
         </div>
