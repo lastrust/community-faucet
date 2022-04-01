8b2995a07d2e2ff1d029415b7be90a278258a14a
inaridiy <inainainariinainari@gmail.com>
2022-04-01 21:12:53 +0900

    広告の試験導入

---
+++ app/src/pages/_document.tsx
@@ -0,0 +1,19 @@
+import { Head, Html, Main, NextScript } from "next/document";
+
+export default function Document() {
+  return (
+    <Html>
+      <Head>
+        <script
+          async
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9592509925467345"
+          crossOrigin="anonymous"
+        ></script>
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}
