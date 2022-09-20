9f7b9dc77d4c8462172fa371c072bb58c90fbc47
Inaridiy <inainainariinainari@gmail.com>
2022-09-20 22:43:23 +0900

    feat: add trpc settings for interface

--- packages/interface/pages/_app.tsx
+++ packages/interface/pages/_app.tsx
@@ -1,6 +1,11 @@
 import '../styles/globals.css';
 import { AppProps } from 'next/app';
 import { NextUIProvider, Spacer } from '@nextui-org/react';
+import { withTRPC } from '@trpc/next';
+import type { AppRouter } from './api/trpc/[trpc]';
+
+
+
 import Layout from 'src/layout/layout';
 import Themes from 'src/layout/themes';
 function MyApp({ Component, pageProps }: AppProps) {
@@ -16,4 +21,16 @@ function MyApp({ Component, pageProps }: AppProps) {
   );
 }
 
-export default MyApp
+export default withTRPC<AppRouter>({
+  config({ ctx }) {
+    const url = process.env.VERCEL_URL
+      ? `https://${process.env.VERCEL_URL}/api/trpc`
+      : 'http://localhost:3000/api/trpc';
+    return {
+      url,
+    };
+  },
+
+  ssr: false,
+})(MyApp);
+
---
+++ packages/interface/src/utils/trpc.ts
@@ -0,0 +1,3 @@
+import { createReactQueryHooks } from '@trpc/react';
+import type { AppRouter } from '../../pages/api/trpc/[trpc]';
+export const trpc = createReactQueryHooks<AppRouter>();
