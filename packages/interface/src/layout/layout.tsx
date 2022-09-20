96d89f798de9124d116a18b4acfd0ce9baf657b9
moons14 <moons@moons14.com>
2022-09-20 20:36:01 +0900

    feat レイアウト追加

---
+++ packages/interface/src/layout/layout.tsx
@@ -0,0 +1,55 @@
+import { ReactNode } from "react";
+import { Navbar, Link, Text, Button } from "@nextui-org/react";
+import { useRouter } from "next/router";
+import { useTheme as useNextTheme } from 'next-themes'
+import { Switch, useTheme } from '@nextui-org/react'
+import { MoonIcon, SunIcon } from "@heroicons/react/24/solid";
+export default function Layout({ children }: { children: ReactNode }) {
+    const router = useRouter();
+    const { setTheme } = useNextTheme();
+    const { isDark, type } = useTheme();
+    return (<>
+        <Navbar shouldHideOnScroll variant="floating" isBordered>
+            <Navbar.Toggle showIn="xs" />
+            <Navbar.Brand
+                css={{
+                    "@xs": {
+                        w: "12%",
+                    },
+                }}
+            >
+                <Link
+                    href="/"
+                    color="text"
+                >
+                    <Text b color="inherit" hideIn="xs">
+                        Community Faucet
+                    </Text>
+                </Link>
+            </Navbar.Brand>
+            <Navbar.Content
+                variant="highlight"
+            >
+                <Navbar.Link isActive={router.pathname == "/"} href="/">Home</Navbar.Link>
+                <Navbar.Link isActive={router.pathname == "/supporters"} href="/supporters">Supporters</Navbar.Link>
+            </Navbar.Content>
+            <Navbar.Content>
+                <Navbar.Link color="inherit" href="#">
+                    <Button
+                        auto
+                        light
+                        color="gradient"
+                        onClick={() => { setTheme(isDark ? "light" : "dark") }}
+                        icon={isDark ? <MoonIcon width={20} /> : <SunIcon width={20} />}
+                    />
+                </Navbar.Link>
+                <Navbar.Item>
+                    <Button auto flat as={Link} href="#">
+                        Connect Wallet
+                    </Button>
+                </Navbar.Item>
+            </Navbar.Content>
+        </Navbar>
+        {children}
+    </>)
+}
