736ce2ff2ba859f45db132362f8cb78fdf00fc75
moons14 <moons@moons14.com>
2022-09-20 23:07:34 +0900

    fix: スペースの大きさを調整

--- packages/interface/pages/_app.tsx
+++ packages/interface/pages/_app.tsx
@@ -1,14 +1,14 @@
-import '../styles/globals.css';
-import { AppProps } from 'next/app';
 import { NextUIProvider, Spacer } from '@nextui-org/react';
+import { AppProps } from 'next/app';
 import Layout from 'src/layout/layout';
 import Themes from 'src/layout/themes';
+import '../styles/globals.css';
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <NextUIProvider>
       <Themes>
         <Layout>
-          <Spacer y={3} />
+          <Spacer y={1} />
           <Component {...pageProps} />
         </Layout>
       </Themes>
