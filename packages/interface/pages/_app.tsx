3b92306ea603e4f493fb3ffcf4488761c9f8436c
moons14 <moons@moons14.com>
2022-09-20 18:33:34 +0900

    chore mantine関連コード削除

--- packages/interface/pages/_app.tsx
+++ packages/interface/pages/_app.tsx
@@ -1,9 +1,8 @@
 import '../styles/globals.css';
-import App_nav from 'src/components/appshell';
 import { AppProps } from 'next/app';
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <App_nav><Component {...pageProps} /></App_nav>
+    <Component {...pageProps} />
   );
 }
 
--- packages/interface/pages/index.tsx
+++ packages/interface/pages/index.tsx
@@ -1,51 +1,8 @@
 import type { NextPage } from 'next'
-import { Container, Grid } from '@mantine/core';
-import Top_networks from 'src/components/top_networks';
 const Home: NextPage = () => {
   return (
     <>
-      <Container >
-        <Grid grow gutter="lg">
-          <Grid.Col xs={12} sm={12} md={12} lg={6} xl={6}>
-            <Top_networks
-              name="aster"
-              balance={630.06}
-              total_drop={1164.73}
-              supporter={197}
-              symbol="ASTR"
-              color="indigo"
-              grow={false}
-            /></Grid.Col>
-          <Grid.Col xs={12} sm={12} md={12} lg={6} xl={6}><Top_networks
-            name="shibuya"
-            balance={1557.24}
-            total_drop={606.80}
-            supporter={69}
-            symbol="SBY"
-            color="gray"
-            grow={false}
-          /></Grid.Col>
-          <Grid.Col xs={12} sm={12} md={12} lg={6} xl={6}><Top_networks
-            name="matic"
-            balance={1.94}
-            total_drop={29.38}
-            supporter={30}
-            symbol="MATIC"
-            color="grape"
-            grow={false}
-          /></Grid.Col>
-          <Grid.Col xs={12} sm={12} md={12} lg={6} xl={6}><Top_networks
-            name="shiden"
-            balance={347.07}
-            total_drop={1145.38}
-            supporter={62}
-            symbol="SDN"
-            color="cyan"
-            grow={false}
-          /></Grid.Col>
-
-        </Grid>
-      </Container>
+      
     </>
   )
 }
