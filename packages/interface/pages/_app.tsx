2638777862c921c30970714b02a5fd5d50a3cc5f
moons14 <moons@moons14.com>
2022-09-20 22:15:27 +0900

    style ヘッダーとコンテンツのスペースを空ける

--- packages/interface/pages/_app.tsx
+++ packages/interface/pages/_app.tsx
@@ -1,6 +1,6 @@
 import '../styles/globals.css';
 import { AppProps } from 'next/app';
-import { NextUIProvider } from '@nextui-org/react';
+import { NextUIProvider, Spacer } from '@nextui-org/react';
 import Layout from 'src/layout/layout';
 import Themes from 'src/layout/themes';
 function MyApp({ Component, pageProps }: AppProps) {
@@ -8,6 +8,7 @@ function MyApp({ Component, pageProps }: AppProps) {
     <NextUIProvider>
       <Themes>
         <Layout>
+          <Spacer y={3} />
           <Component {...pageProps} />
         </Layout>
       </Themes>
