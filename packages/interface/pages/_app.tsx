5d08102599ce00a8502ef03503ae65b519dd73f1
moons14 <moons@moons14.com>
2022-09-20 20:36:25 +0900

    feat テーマ・レイアウト適用

--- packages/interface/pages/_app.tsx
+++ packages/interface/pages/_app.tsx
@@ -1,10 +1,16 @@
 import '../styles/globals.css';
 import { AppProps } from 'next/app';
 import { NextUIProvider } from '@nextui-org/react';
+import Layout from 'src/layout/layout';
+import Themes from 'src/layout/themes';
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <NextUIProvider>
-      <Component {...pageProps} />
+      <Themes>
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </Themes>
     </NextUIProvider>
   );
 }
